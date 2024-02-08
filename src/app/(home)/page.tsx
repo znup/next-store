d717d0798c7ef0530ef746cf11b9ff4ad1908483
SirAdan <lpelcastre@bomuca.com>
2024-02-08 14:17:24 -0800

    [ADD] seo

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,18 @@
+import { Metadata } from 'next';
 import { MainProducts } from '..';
+
+export const metadata: Metadata = {
+  title: '⚡Future Store',
+  description:
+    'Bienvenido a la tienda del futuro.🚀 un e-commerce de otro mundo🌎',
+  keywords: [
+    'ecommerce',
+    'tienda',
+    'tienda en linea',
+    'tienda virtual',
+    'tecnologia',
+  ],
+};
 
 export default function Home() {
   return (
