15476c554981deeb03b738a4049b0ce9f1bab630
SirAdan <lpelcastre@bomuca.com>
2024-02-12 12:57:23 -0800

    [ADD] items cart

--- src/app/components/shared/ShoppingCart/ShoppingCart.tsx
+++ src/app/components/shared/ShoppingCart/ShoppingCart.tsx
@@ -4,14 +4,28 @@ import { FaShoppingCart } from 'react-icons/fa';
 import { useShoppingCart } from 'app/hooks/useShoppingCart';
 
 import styles from './ShoppingCart.module.sass';
+import { useState } from 'react';
 
 export const ShoppingCart = () => {
   const { cart } = useShoppingCart();
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOpen = () => setIsOpen(!isOpen);
 
   return (
-    <button className={styles.ShoppingCart}>
+    <button className={styles.ShoppingCart} onClick={handleOpen}>
       <span className={styles.ShoppingCart__counter}>{cart.length}</span>
       <FaShoppingCart />
+      {isOpen && (
+        <div className={styles.ShoppingCart__items}>
+          {cart.map((item) => (
+            <>
+              <p key={item?.id}>{item?.title}</p>
+              <p>Cantidad: {item.quantity}</p>
+            </>
+          ))}
+          <button className={styles.ShoppingCart__buyButton}>Comprar</button>
+        </div>
+      )}
     </button>
   );
 };
